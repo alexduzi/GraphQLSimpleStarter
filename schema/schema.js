52007ecf449253fcd653c3d52c9419f0a4a8248c
Alex Duzi <duzihd@gmail.com>
2017-06-05 15:54:26 -0300

    First commit GrapahQL with react simple starter

---
+++ schema/schema.js
@@ -0,0 +1,58 @@
+const _ = require('lodash');
+const axios = require('axios');
+const graphql = require('graphql');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLNonNull
+} = graphql;
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                                .then(res => res.data);
+            }
+        }
+    }
+});
+
+const UserQuery = {
+  type: UserType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLString) }
+  },
+  resolve(parentValue, args) {
+    return axios.get(`http://localhost:3000/users/${args.id}`)
+        .then(res => res.data);
+  }
+}
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields:{
+    user: UserQuery
+  }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
